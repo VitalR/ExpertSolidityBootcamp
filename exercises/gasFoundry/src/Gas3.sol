8c41c3739e137d7a6356a74c9e94b2a6d36d240b
VitalR <vitalii.rodikov@gmail.com>
2023-10-25 23:53:03 +0300

    feat: add contracts with max optimization

---
+++ exercises/gasFoundry/src/Gas3.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract GasContract3 {
+     uint256 private lastAmount;
+
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public whitelist;
+
+    address[5] public administrators;
+    constructor(address[] memory _admins, uint256 _totalSupply) {
+        // for (uint256 ii; ii < 5; ) {
+        //     administrators[ii] = _admins[ii];
+        // }
+        assembly {
+            for {let ii} lt(ii, 5) {ii := add(ii, 1)} {
+                let admin := mload(add(_admins, mul(0x20, add(ii, 1))))
+                sstore(add(administrators.slot, ii), admin)
+                //administrators[ii] = admin;
+            }
+            mstore(0x0, caller())
+            mstore(0x20, balances.slot)
+            let slot := keccak256(0x0, 0x40)
+            sstore(slot, _totalSupply)
+            //balances[msg.sender] = _totalSupply;
+        }
+    }
+
+    function balanceOf(address _user) public view returns (uint256) {
+        return balances[_user];
+    }
+
+    function transfer(
+        address _recipient,
+        uint256 _amount,
+        string calldata _name
+    ) public {
+        assembly {
+            //balances[msg.sender] -= _amount;
+            let ptr := mload(0x40)
+            mstore(ptr, caller())
+            mstore(add(ptr, 0x20), balances.slot)
+            let senderSlot := keccak256(ptr, 0x40)
+            sstore(senderSlot, sub(sload(senderSlot), _amount))
+            let val := sload(senderSlot)
+
+            //balances[_recipient] += _amount;
+            mstore(ptr, _recipient)
+            mstore(add(ptr, 0x20), balances.slot)
+            let recipientSlot := keccak256(ptr, 0x40)
+            sstore(recipientSlot, add(sload(recipientSlot), _amount))
+        }
+    }
+
+    function addToWhitelist(address _userAddrs, uint256 _tier) public {
+        // if (_tier >= 255) revert TierLevelTooHigh(_tier);
+        // uint256 ii;
+        // bool revertV = false;
+        // for (ii= 0; ii < 5; ii++) {
+        //     if (administrators[ii] == msg.sender){
+        //         revertV = true;
+        //         break;}
+        // }
+        // if (revertV) revert CallerNotAdmin(msg.sender);
+
+        // emit AddedToWhitelist(_userAddrs, _tier);
+
+        assembly {
+            if gt(_tier, 255) {revert(0,0)}
+            let ii := 0
+            for {} lt(ii, 5) {ii := add(ii, 1)} {
+                if eq(sload(add(administrators.slot, ii)), caller()) {
+                    break
+                }
+            }
+            if eq(ii, 5) {revert(0,0)}
+            let signature := 0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52
+            mstore(0, _userAddrs)
+            mstore(0x20, _tier)
+            log1(0, 0x40, signature)
+            // let ptr := mload(0x40)
+            // mstore(ptr, _userAddrs)
+            // mstore(add(ptr, 0x20), _tier)
+            // log1(ptr, 0x40, signature)
+
+        }
+        //if (ii == 5) revert CallerNotAdmin(msg.sender);
+
+        //emit AddedToWhitelist(_userAddrs, _tier);
+    }
+
+    function whiteTransfer(address _recipient, uint256 _amount) public {
+        assembly {
+            sstore(lastAmount.slot, _amount)
+
+            let ptr := mload(0x40)
+            mstore(ptr, caller())
+            mstore(add(ptr, 0x20), balances.slot)
+            let senderSlot := keccak256(ptr, 0x40)
+            sstore(senderSlot, sub(sload(senderSlot), _amount))
+
+            mstore(ptr, _recipient)
+            mstore(add(ptr, 0x20), balances.slot)
+            let recipientSlot := keccak256(ptr, 0x40)
+            sstore(recipientSlot, add(sload(recipientSlot), _amount))
+
+            let signature := 0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3
+            log2(0x0, 0x0, signature, _recipient)
+        }
+        // lastAmount = _amount;
+
+        //balances[msg.sender] -= _amount;
+        //balances[_recipient] += _amount;
+
+        //emit WhiteListTransfer(_recipient, _amount);
+    }
+
+    function getPaymentStatus(
+        address sender
+    ) public view returns (bool, uint256) {
+        return (true, lastAmount);
+    }
+}
---
+++ exercises/gasFoundry/src/Gas4.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract GasContract4 {
+    uint256 private constant BALANCE = 100;
+    uint256 private _lastAmount;
+
+    // keccak256("AddedToWhitelist(address,uint256)")
+    bytes32 private constant ADDED_TO_WHITELIST_EVENT_SIGNATURE =
+        0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52;
+    // keccak256("WhiteListTransfer(address)")
+    bytes32 private constant WHITELIST_TRANSFER_EVENT_SIGNATURE =
+        0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3;
+
+    event AddedToWhitelist(address userAddress, uint256 tier);
+    event WhiteListTransfer(address indexed recipient);
+
+    constructor(address[] memory, uint256) payable { }
+
+    function transfer(address, uint256, string calldata) external payable { }
+
+    function addToWhitelist(address _userAddrs, uint256 _tier) external payable {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Revert if msg.sender is not 0x1234 (contract owner) or tier is greater than 254.
+            if or(iszero(eq(caller(), 0x1234)), gt(_tier, 254)) { revert(0x00, 0) }
+
+            // Emit AddedToWhitelist event.
+            mstore(0x00, _userAddrs)
+            mstore(0x20, _tier)
+            log1(0x00, 64, ADDED_TO_WHITELIST_EVENT_SIGNATURE)
+        }
+    }
+
+    function whiteTransfer(address _recipient, uint256 _amount) external payable {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Store _amount in _lastAmount.
+            sstore(_lastAmount.slot, _amount)
+            // Emit WhitelistTransfer event.
+            log2(0x00, 0, WHITELIST_TRANSFER_EVENT_SIGNATURE, _recipient)
+        }
+    }
+
+    function balances(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // lastAmount = _lastAmount;
+            let lastAmount := sload(_lastAmount.slot)
+            // If lastAmount is 0.
+            if iszero(lastAmount) {
+                // Return BALANCE.
+                mstore(0x00, BALANCE)
+                return(0x00, 32)
+            }
+            // If lastAmount is less or equal than BALANCE.
+            if lt(lastAmount, add(BALANCE, 1)) {
+                mstore(0x00, sub(BALANCE, lastAmount))
+                // _lastAmount += BALANCE;
+                sstore(_lastAmount.slot, add(lastAmount, BALANCE))
+                return(0x00, 32)
+            }
+            // Else return lastAmount.
+            mstore(0x00, lastAmount)
+            return(0x00, 32)
+        }
+    }
+
+    function getPaymentStatus(address) external payable returns (bool, uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return (true, _lastAmount).
+            mstore(0x00, 1)
+            mstore(0x20, sload(_lastAmount.slot))
+            return(0x00, 64)
+        }
+    }
+
+    function administrators(uint256 _index) external payable returns (address) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            switch _index
+            // If _index == 0 return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2.
+            case 0 {
+                mstore(0x00, 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2)
+                return(0x00, 32)
+            }
+            // If _index == 1 return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46.
+            case 1 {
+                mstore(0x00, 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46)
+                return(0x00, 32)
+            }
+            // If _index == 2 return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf.
+            case 2 {
+                mstore(0x00, 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf)
+                return(0x00, 32)
+            }
+            // If _index == 3 return 0xeadb3d065f8d15cc05e92594523516aD36d1c834.
+            case 3 {
+                mstore(0x00, 0xeadb3d065f8d15cc05e92594523516aD36d1c834)
+                return(0x00, 32)
+            }
+            // Else return 0x1234.
+            default {
+                mstore(0x00, 0x1234)
+                return(0x00, 32)
+            }
+        }
+    }
+
+    function balanceOf(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return BALANCE.
+            mstore(0x00, BALANCE)
+            return(0x00, 32)
+        }
+    }
+
+    function whitelist(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return 0.
+            return(0x00, 32)
+        }
+    }
+}
